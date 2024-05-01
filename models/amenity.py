ca3ba596ef52455e8b53792f0952fb8fa1fe69ec
Ahmed Mohamed <ahmed.mohamed91196@gmail.com>
2024-05-01 11:22:16 -0700

    import foreignkey & table to handle relationship

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Amenity Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey, Table
 from sqlalchemy.orm import relationship
 
 
