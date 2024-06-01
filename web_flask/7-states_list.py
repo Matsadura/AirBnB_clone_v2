cf741687d2eff665a48c0f7615432144651c1a11
Matsadura <zidanditoo1@gmail.com>
2024-06-01 12:56:49 +0100

    TEST 5

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,82 +1,93 @@
 #!/usr/bin/python3
-""" Starts a Flask Web Application """
-from flask import Flask, render_template, abort
+"""
+script starts Flask web app
+    listen on 0.0.0.0, port 5000
+    routes: /:                    display "Hello HBNB!"
+            /hbnb:                display "HBNB"
+            /c/<text>:            display "C" + text (replace "_" with " ")
+            /python/<text>:       display "Python" + text (default="is cool")
+            /number/<n>:          display "n is a number" only if int
+            /number_template/<n>: display HTML page only if n is int
+            /number_odd_or_even/<n>: display HTML page; display odd/even info
+            /states_list:         display HTML and state info from storage;
+"""
 from models import storage
+from models import *
+from flask import Flask, render_template
 from models.state import State
-
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-def get_storage(cls=None):
-    """ Returns the database """
-    return storage.all(cls)
-
-
-@app.teardown_appcontext
-def remove_sqlsession(exception=None):
-    """ Hands the app.teardown """
-    storage.close()
-
-
-@app.route("/", strict_slashes=False)
-def root():
-    """ Displays Hello HBNB """
+@app.route('/')
+def hello_hbnb():
+    """display text"""
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
-    """ Displays HBNB """
+    """display text"""
     return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def c(text):
-    """ Returns C """
-    text = text.replace('_', ' ')
-    return f"C {escape(text)}"
+@app.route('/c/<text>')
+def c_text(text):
+    """display custom text given"""
+    return "C {}".format(text.replace('_', ' '))
 
 
-@app.route("/python", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def python(text="is cool"):
-    """ Returns Python """
-    text = text.replace('_', ' ')
-    return f"Python {escape(text)}"
+@app.route('/python')
+@app.route('/python/<text>')
+def python_text(text="is cool"):
+    """display custom text given
+       first route statement ensures it works for:
+          curl -Ls 0.0.0.0:5000/python ; echo "" | cat -e
+          curl -Ls 0.0.0.0:5000/python/ ; echo "" | cat -e
+    """
+    return "Python {}".format(text.replace('_', ' '))
 
 
-@app.route("/number/<n>", strict_slashes=False)
-def is_number(n):
-    """ Displays 'n is number' only if n is an integer """
-    if n.isdigit():
-        return f"{escape(n)} is a number"
-    abort(404)
+@app.route('/number/<int:n>')
+def text_if_int(n):
+    """display text only if int given"""
+    return "{:d} is a number".format(n)
 
 
-@app.route("/number_template/<n>", strict_slashes=False)
-def Number(n):
-    """ Displays a HTML page only if n is an integer """
-    if n.isdigit():
-        return render_template('5-number.html', n=escape(n))
-    abort(404)
+@app.route('/number_template/<int:n>')
+def html_if_int(n):
+    """display html page only if int given
+       place given int into html template
+    """
+    return render_template('5-number.html', n=n)
 
 
-@app.route("/number_odd_or_even/<n>", strict_slashes=False)
-def number_odd_even(n):
-    """ Returns if the number is odd or even """
-    if n.isdigit():
-        return render_template('6-number_odd_or_even.html', n=eval(escape(n)))
-    abort(404)
+@app.route('/number_odd_or_even/<int:n>')
+def html_odd_or_even(n):
+    """display html page only if int given
+       place given int into html template
+       substitute text to display if int is odd or even
+    """
+    odd_or_even = "even" if (n % 2 == 0) else "odd"
+    return render_template('6-number_odd_or_even.html',
+                           n=n, odd_or_even=odd_or_even)
+
+
+@app.teardown_appcontext
+def tear_down(self):
+    """after each request remove current SQLAlchemy session"""
+    storage.close()
 
 
 @app.route('/states_list')
-def states_list():
-    """ Displays the states sorted by name """
-    states_values = get_storage(State).values()
-    sorted_states = sorted(states_values, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=sorted_states)
+def html_fetch_states():
+    """display html page
+       fetch sorted states to insert into html in UL tag
+    """
+    state_objs = storage.all(State)
+    return render_template('7-states_list.html',
+                           state_objs=state_objs.values())
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
