1808a7653ff5864d8c6bc2ec0f82088bbe1dd214
Matsadura <zidanditoo1@gmail.com>
2024-06-01 12:06:42 +0100

    [UPDATE] Add abort

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Starts a Flask Web Application """
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 from models import storage
 from models.state import State
 
